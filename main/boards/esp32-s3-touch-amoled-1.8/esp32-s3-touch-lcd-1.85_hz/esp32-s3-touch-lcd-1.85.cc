232c7b9bddb8efd2110710953b6f9ae7de163ec5
Fairy xu <feilibaby@163.com>
2025-03-25 19:13:15 +0800

    feat:1.85lcd

--- main/boards/esp32-s3-touch-amoled-1.8/esp32-s3-touch-lcd-1.85_hz/esp32-s3-touch-lcd-1.85.cc
+++ main/boards/esp32-s3-touch-amoled-1.8/esp32-s3-touch-lcd-1.85_hz/esp32-s3-touch-lcd-1.85.cc
@@ -6,12 +6,9 @@
 #include "button.h"
 #include "config.h"
 #include "iot/thing_manager.h"
-#include "led/single_led.h"
-#include "assets/lang_config.h"
 
 #include <esp_log.h>
 #include "i2c_device.h"
-#include <esp_log.h>
 #include <driver/i2c.h>
 #include <driver/ledc.h>
 #include <wifi_station.h>
@@ -21,8 +18,7 @@
 #include <esp_timer.h>
 #include "esp_io_expander_tca9554.h"
 
-
-#define TAG "CompactWifiBoard"
+#define TAG "waveshare_lcd_1_85"
 
 #define LCD_OPCODE_WRITE_CMD        (0x02ULL)
 #define LCD_OPCODE_READ_CMD         (0x0BULL)
@@ -30,6 +26,7 @@
 
 LV_FONT_DECLARE(font_puhui_16_4);
 LV_FONT_DECLARE(font_awesome_16_4);
+
 static const st77916_lcd_init_cmd_t vendor_specific_init_new[] = {
     {0xF0, (uint8_t []){0x28}, 1, 0},
     {0xF2, (uint8_t []){0x28}, 1, 0},
@@ -217,38 +214,30 @@ static const st77916_lcd_init_cmd_t vendor_specific_init_new[] = {
     {0x11, (uint8_t []){0x00}, 1, 120},
     {0x29, (uint8_t []){0x00}, 1, 0},  
 };
-
-class CompactWifiBoard : public WifiBoard {
-private: 
+class CustomBoard : public WifiBoard {
+private:
     i2c_master_bus_handle_t i2c_bus_;
     esp_io_expander_handle_t io_expander = NULL;
-    LcdDisplay* display_; 
-    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
-    esp_lcd_panel_handle_t panel_ = nullptr; 
-    Button boot_button_;
-    Button touch_button_;
-    Button volume_up_button_;
-    Button volume_down_button_; 
+    LcdDisplay* display_;
     button_handle_t boot_btn,pwr_btn;
 
     void InitializeI2c() {
         // Initialize I2C peripheral
         i2c_master_bus_config_t i2c_bus_cfg = {
             .i2c_port = (i2c_port_t)0,
-            .sda_io_num = I2C_SDA_IO, //GPIO_NUM_41
-            .scl_io_num = I2C_SCL_IO, //GPIO_NUM_42 这两两个都试一下
+            .sda_io_num = I2C_SDA_IO,
+            .scl_io_num = I2C_SCL_IO,
             .clk_source = I2C_CLK_SRC_DEFAULT,
         };
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &i2c_bus_));
     }
-
+    
     void InitializeTca9554(void) {
         esp_err_t ret = esp_io_expander_new_i2c_tca9554(i2c_bus_, I2C_ADDRESS, &io_expander);
         if(ret != ESP_OK)
-            ESP_LOGE(TAG, "TCA9554 create returned error");  
+            ESP_LOGE(TAG, "TCA9554 create returned error");        
         gpio_set_level(QSPI_PIN_NUM_LCD_RST, 1);    
-        gpio_set_level(TP_PIN_NUM_RST, 1);          
-
+        gpio_set_level(TP_PIN_NUM_RST, 1);    
         // uint32_t input_level_mask = 0;
         // ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_INPUT);               // 设置引脚 EXIO0 和 EXIO1 模式为输入 
         // ret = esp_io_expander_get_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, &input_level_mask);             // 获取引脚 EXIO0 和 EXIO1 的电平状态,存放在 input_level_mask 中
@@ -257,20 +246,23 @@ private:
         // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_2 | IO_EXPANDER_PIN_NUM_3, 1);                             // 将引脚电平设置为 1
         // ret = esp_io_expander_print_state(io_expander);                                                                             // 打印引脚状态
 
-        ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_OUTPUT);                 // 设置引脚 EXIO0 和 EXIO1 模式为输出
-        ESP_ERROR_CHECK(ret);
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
+        //ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_OUTPUT);                 // 设置引脚 EXIO0 和 EXIO1 模式为输出
+        //ESP_ERROR_CHECK(ret);
+       // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
+       // ESP_ERROR_CHECK(ret);
+        //vTaskDelay(pdMS_TO_TICKS(300));
+       // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 0);                                // 复位 LCD 与 TouchPad
+       // ESP_ERROR_CHECK(ret);
         vTaskDelay(pdMS_TO_TICKS(300));
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 0);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
-        vTaskDelay(pdMS_TO_TICKS(300));
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
+        gpio_set_level(QSPI_PIN_NUM_LCD_RST, 0);    
+        gpio_set_level(TP_PIN_NUM_RST, 0);    
+        //ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
+      //  ESP_ERROR_CHECK(ret);
     }
 
     void InitializeSpi() {
-        ESP_LOGI(TAG, "Initialize QSPI bus"); 
+        ESP_LOGI(TAG, "Initialize QSPI bus");
+
         const spi_bus_config_t bus_config = TAIJIPI_ST77916_PANEL_BUS_QSPI_CONFIG(QSPI_PIN_NUM_LCD_PCLK,
                                                                         QSPI_PIN_NUM_LCD_DATA0,
                                                                         QSPI_PIN_NUM_LCD_DATA1,
@@ -286,30 +278,16 @@ private:
         esp_lcd_panel_handle_t panel = nullptr;
 
         ESP_LOGI(TAG, "Install panel IO");
-
-        // 在Initializest77916Display()中添加硬件复位
-        gpio_config_t rst_config = {
-            .pin_bit_mask = (1ULL << QSPI_PIN_NUM_LCD_RST),
-            .mode = GPIO_MODE_OUTPUT
-        };
-        ESP_ERROR_CHECK(gpio_config(&rst_config));
-
-        // 标准复位时序
-        gpio_set_level(QSPI_PIN_NUM_LCD_RST, 0);
-        vTaskDelay(pdMS_TO_TICKS(50));
-        gpio_set_level(QSPI_PIN_NUM_LCD_RST, 1);
-        vTaskDelay(pdMS_TO_TICKS(120));
-
         
         esp_lcd_panel_io_spi_config_t io_config = {
             .cs_gpio_num = QSPI_PIN_NUM_LCD_CS,               
             .dc_gpio_num = -1,                  
-            .spi_mode = 3,                     
-            .pclk_hz = 80 * 1000 * 1000,      
+            .spi_mode = 0,                     
+            .pclk_hz = 3 * 1000 * 1000,      
             .trans_queue_depth = 10,            
             .on_color_trans_done = NULL,                            
             .user_ctx = NULL,                   
-            .lcd_cmd_bits = 8,                 
+            .lcd_cmd_bits = 32,                 
             .lcd_param_bits = 8,                
             .flags = {                          
             .dc_low_on_data = 0,            
@@ -388,51 +366,71 @@ private:
                                     });
     }
  
+    void InitializeButtonsCustom() {
+        gpio_reset_pin(BOOT_BUTTON_GPIO);                                     
+        gpio_set_direction(BOOT_BUTTON_GPIO, GPIO_MODE_INPUT);   
+        gpio_reset_pin(PWR_BUTTON_GPIO);                                     
+        gpio_set_direction(PWR_BUTTON_GPIO, GPIO_MODE_INPUT);   
+        gpio_reset_pin(PWR_Control_PIN);                                     
+        gpio_set_direction(PWR_Control_PIN, GPIO_MODE_OUTPUT);    
+        // gpio_set_level(PWR_Control_PIN, false);
+        gpio_set_level(PWR_Control_PIN, true); 
+    }
     void InitializeButtons() {
-        boot_button_.OnClick([this]() {
+        InitializeButtonsCustom();
+        button_config_t btns_config = {
+            .type = BUTTON_TYPE_CUSTOM,
+            .long_press_time = 2000,
+            .short_press_time = 50,
+            .custom_button_config = {
+                .active_level = 0,
+                .button_custom_init = nullptr,
+                .button_custom_get_key_value = [](void *param) -> uint8_t {
+                    return gpio_get_level(BOOT_BUTTON_GPIO);
+                },
+                .button_custom_deinit = nullptr,
+                .priv = this,
+            },
+        };
+        boot_btn = iot_button_create(&btns_config);
+        iot_button_register_cb(boot_btn, BUTTON_SINGLE_CLICK, [](void* button_handle, void* usr_data) {
+            auto self = static_cast<CustomBoard*>(usr_data);
             auto& app = Application::GetInstance();
             if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
-                ResetWifiConfiguration();
+                self->ResetWifiConfiguration();
             }
             app.ToggleChatState();
-        });
-        touch_button_.OnPressDown([this]() {
-            Application::GetInstance().StartListening();
-        });
-        touch_button_.OnPressUp([this]() {
-            Application::GetInstance().StopListening();
-        });
+        }, this);
+        iot_button_register_cb(boot_btn, BUTTON_LONG_PRESS_START, [](void* button_handle, void* usr_data) {
+            // 长按无处理
+        }, this);
 
-        volume_up_button_.OnClick([this]() {
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() + 10;
-            if (volume > 100) {
-                volume = 100;
+        btns_config.long_press_time = 5000;
+        btns_config.custom_button_config.button_custom_get_key_value = [](void *param) -> uint8_t {
+            return gpio_get_level(PWR_BUTTON_GPIO);
+        };
+        pwr_btn = iot_button_create(&btns_config);
+        iot_button_register_cb(pwr_btn, BUTTON_SINGLE_CLICK, [](void* button_handle, void* usr_data) {
+            // auto self = static_cast<CustomBoard*>(usr_data);                                     // 以下程序实现供用户参考 ，实现单击pwr按键调整亮度               
+            // if(self->GetBacklight()->brightness() > 1)                                           // 如果亮度不为0
+            //     self->GetBacklight()->SetBrightness(1);                                          // 设置亮度为1         
+            // else
+            //     self->GetBacklight()->RestoreBrightness();                                       // 恢复原本亮度
+            // 短按无处理
+        }, this);
+        iot_button_register_cb(pwr_btn, BUTTON_LONG_PRESS_START, [](void* button_handle, void* usr_data) {
+            auto self = static_cast<CustomBoard*>(usr_data);
+            if(self->GetBacklight()->brightness() > 0) {
+                self->GetBacklight()->SetBrightness(0);
+                gpio_set_level(PWR_Control_PIN, false);
             }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
-        });
-
-        volume_up_button_.OnLongPress([this]() {
-            GetAudioCodec()->SetOutputVolume(100);
-            GetDisplay()->ShowNotification(Lang::Strings::MAX_VOLUME);
-        });
-
-        volume_down_button_.OnClick([this]() {
-            auto codec = GetAudioCodec();
-            auto volume = codec->output_volume() - 10;
-            if (volume < 0) {
-                volume = 0;
+            else {
+                self->GetBacklight()->RestoreBrightness();
+                gpio_set_level(PWR_Control_PIN, true);
             }
-            codec->SetOutputVolume(volume);
-            GetDisplay()->ShowNotification(Lang::Strings::VOLUME + std::to_string(volume));
-        });
-
-        volume_down_button_.OnLongPress([this]() {
-            GetAudioCodec()->SetOutputVolume(0);
-            GetDisplay()->ShowNotification(Lang::Strings::MUTED);
-        });
+        }, this);
     }
+
 
     // 物联网初始化，添加对 AI 可见设备
     void InitializeIot() {
@@ -442,39 +440,31 @@ private:
     }
 
 public:
-    CompactWifiBoard() : 
-        boot_button_(BOOT_BUTTON_GPIO),
-        touch_button_(TOUCH_BUTTON_GPIO),
-        volume_up_button_(VOLUME_UP_BUTTON_GPIO),
-        volume_down_button_(VOLUME_DOWN_BUTTON_GPIO) {
+    CustomBoard() {   
         InitializeI2c();
         InitializeTca9554();
-        InitializeSpi(); 
+        InitializeSpi();
+        Initializest77916Display();
         InitializeButtons();
         InitializeIot();
         GetBacklight()->RestoreBrightness();
     }
- 
 
     virtual AudioCodec* GetAudioCodec() override {
-#ifdef AUDIO_I2S_METHOD_SIMPLEX
         static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
-#else
-        static NoAudioCodecDuplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
-#endif
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, I2S_STD_SLOT_BOTH, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN, I2S_STD_SLOT_RIGHT); // I2S_STD_SLOT_LEFT / I2S_STD_SLOT_RIGHT / I2S_STD_SLOT_BOTH
+
         return &audio_codec;
     }
 
     virtual Display* GetDisplay() override {
         return display_;
     }
-
+    
     virtual Backlight* GetBacklight() override {
         static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
         return &backlight;
     }
 };
 
-DECLARE_BOARD(CompactWifiBoard);
+DECLARE_BOARD(CustomBoard);
--- main/boards/esp32-s3-touch-lcd-1.85_hz/esp32-s3-touch-lcd-1.85.cc
+++ main/boards/esp32-s3-touch-lcd-1.85_hz/esp32-s3-touch-lcd-1.85.cc
@@ -4,7 +4,9 @@
 #include "system_reset.h"
 #include "application.h"
 #include "button.h"
-#include "config.h" 
+#include "config.h"
+#include "iot/thing_manager.h"
+
 #include <esp_log.h>
 #include "i2c_device.h"
 #include <driver/i2c.h>
@@ -16,7 +18,7 @@
 #include <esp_timer.h>
 #include "esp_io_expander_tca9554.h"
 
-#define TAG "waveshare_lcd_1_85c"
+#define TAG "waveshare_lcd_1_85"
 
 #define LCD_OPCODE_WRITE_CMD        (0x02ULL)
 #define LCD_OPCODE_READ_CMD         (0x0BULL)
@@ -212,13 +214,12 @@ static const st77916_lcd_init_cmd_t vendor_specific_init_new[] = {
     {0x11, (uint8_t []){0x00}, 1, 120},
     {0x29, (uint8_t []){0x00}, 1, 0},  
 };
-
 class CustomBoard : public WifiBoard {
 private:
-    Button boot_button_;
     i2c_master_bus_handle_t i2c_bus_;
     esp_io_expander_handle_t io_expander = NULL;
     LcdDisplay* display_;
+    button_handle_t boot_btn,pwr_btn;
 
     void InitializeI2c() {
         // Initialize I2C peripheral
@@ -228,24 +229,35 @@ private:
             .scl_io_num = I2C_SCL_IO,
             .clk_source = I2C_CLK_SRC_DEFAULT,
         };
-        //ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &i2c_bus_));
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &i2c_bus_));
     }
     
-    void InitializeTca9554(void)
-    {
+    void InitializeTca9554(void) {
         esp_err_t ret = esp_io_expander_new_i2c_tca9554(i2c_bus_, I2C_ADDRESS, &io_expander);
         if(ret != ESP_OK)
-            ESP_LOGE(TAG, "TCA9554 create returned error");   
-        ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_OUTPUT);                 // 设置引脚 EXIO0 和 EXIO1 模式为输出
-        ESP_ERROR_CHECK(ret);
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
+            ESP_LOGE(TAG, "TCA9554 create returned error");        
+        gpio_set_level(QSPI_PIN_NUM_LCD_RST, 1);    
+        gpio_set_level(TP_PIN_NUM_RST, 1);    
+        // uint32_t input_level_mask = 0;
+        // ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_INPUT);               // 设置引脚 EXIO0 和 EXIO1 模式为输入 
+        // ret = esp_io_expander_get_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, &input_level_mask);             // 获取引脚 EXIO0 和 EXIO1 的电平状态,存放在 input_level_mask 中
+
+        // ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_2 | IO_EXPANDER_PIN_NUM_3, IO_EXPANDER_OUTPUT);              // 设置引脚 EXIO2 和 EXIO3 模式为输出
+        // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_2 | IO_EXPANDER_PIN_NUM_3, 1);                             // 将引脚电平设置为 1
+        // ret = esp_io_expander_print_state(io_expander);                                                                             // 打印引脚状态
+
+        //ret = esp_io_expander_set_dir(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, IO_EXPANDER_OUTPUT);                 // 设置引脚 EXIO0 和 EXIO1 模式为输出
+        //ESP_ERROR_CHECK(ret);
+       // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
+       // ESP_ERROR_CHECK(ret);
+        //vTaskDelay(pdMS_TO_TICKS(300));
+       // ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 0);                                // 复位 LCD 与 TouchPad
+       // ESP_ERROR_CHECK(ret);
         vTaskDelay(pdMS_TO_TICKS(300));
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 0);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
-        vTaskDelay(pdMS_TO_TICKS(300));
-        ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
-        ESP_ERROR_CHECK(ret);
+        gpio_set_level(QSPI_PIN_NUM_LCD_RST, 0);    
+        gpio_set_level(TP_PIN_NUM_RST, 0);    
+        //ret = esp_io_expander_set_level(io_expander, IO_EXPANDER_PIN_NUM_0 | IO_EXPANDER_PIN_NUM_1, 1);                                // 复位 LCD 与 TouchPad
+      //  ESP_ERROR_CHECK(ret);
     }
 
     void InitializeSpi() {
@@ -259,12 +271,14 @@ private:
                                                                         QSPI_LCD_H_RES * 80 * sizeof(uint16_t));
         ESP_ERROR_CHECK(spi_bus_initialize(QSPI_LCD_HOST, &bus_config, SPI_DMA_CH_AUTO));
     }
+
     void Initializest77916Display() {
+
         esp_lcd_panel_io_handle_t panel_io = nullptr;
         esp_lcd_panel_handle_t panel = nullptr;
 
         ESP_LOGI(TAG, "Install panel IO");
-
+        
         esp_lcd_panel_io_spi_config_t io_config = {
             .cs_gpio_num = QSPI_PIN_NUM_LCD_CS,               
             .dc_gpio_num = -1,                  
@@ -293,6 +307,7 @@ private:
                 .use_qspi_interface = 1,
             },
         };
+        
         printf("-------------------------------------- Version selection -------------------------------------- \r\n");
         esp_err_t ret;
         int lcd_cmd = 0x04;
@@ -309,7 +324,7 @@ private:
         } 
         // panel_io_spi_del(io_handle);
         io_config.pclk_hz = 80 * 1000 * 1000;
-        if (esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)QSPI_LCD_HOST, &io_config, &panel_io) != ESP_OK) {
+        if (esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)QSPI_LCD_HOST, &io_config, &panel_io) != ESP_OK){
             printf("Failed to set LCD communication parameters -- SPI\r\n");
             return ;
         }
@@ -327,6 +342,7 @@ private:
             printf("Vendor-specific initialization for case 2.\n");
         }
         printf("------------------------------------- End of version selection------------------------------------- \r\n");
+ 
         const esp_lcd_panel_dev_config_t panel_config = {
             .reset_gpio_num = QSPI_PIN_NUM_LCD_RST,
             .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,     // Implemented by LCD command `36h`
@@ -349,30 +365,94 @@ private:
                                         .emoji_font = font_emoji_64_init(),
                                     });
     }
+ 
+    void InitializeButtonsCustom() {
+        gpio_reset_pin(BOOT_BUTTON_GPIO);                                     
+        gpio_set_direction(BOOT_BUTTON_GPIO, GPIO_MODE_INPUT);   
+        gpio_reset_pin(PWR_BUTTON_GPIO);                                     
+        gpio_set_direction(PWR_BUTTON_GPIO, GPIO_MODE_INPUT);   
+        gpio_reset_pin(PWR_Control_PIN);                                     
+        gpio_set_direction(PWR_Control_PIN, GPIO_MODE_OUTPUT);    
+        // gpio_set_level(PWR_Control_PIN, false);
+        gpio_set_level(PWR_Control_PIN, true); 
+    }
     void InitializeButtons() {
-        boot_button_.OnClick([this]() {
+        InitializeButtonsCustom();
+        button_config_t btns_config = {
+            .type = BUTTON_TYPE_CUSTOM,
+            .long_press_time = 2000,
+            .short_press_time = 50,
+            .custom_button_config = {
+                .active_level = 0,
+                .button_custom_init = nullptr,
+                .button_custom_get_key_value = [](void *param) -> uint8_t {
+                    return gpio_get_level(BOOT_BUTTON_GPIO);
+                },
+                .button_custom_deinit = nullptr,
+                .priv = this,
+            },
+        };
+        boot_btn = iot_button_create(&btns_config);
+        iot_button_register_cb(boot_btn, BUTTON_SINGLE_CLICK, [](void* button_handle, void* usr_data) {
+            auto self = static_cast<CustomBoard*>(usr_data);
             auto& app = Application::GetInstance();
             if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
-                ResetWifiConfiguration();
+                self->ResetWifiConfiguration();
             }
             app.ToggleChatState();
-        });
+        }, this);
+        iot_button_register_cb(boot_btn, BUTTON_LONG_PRESS_START, [](void* button_handle, void* usr_data) {
+            // 长按无处理
+        }, this);
+
+        btns_config.long_press_time = 5000;
+        btns_config.custom_button_config.button_custom_get_key_value = [](void *param) -> uint8_t {
+            return gpio_get_level(PWR_BUTTON_GPIO);
+        };
+        pwr_btn = iot_button_create(&btns_config);
+        iot_button_register_cb(pwr_btn, BUTTON_SINGLE_CLICK, [](void* button_handle, void* usr_data) {
+            // auto self = static_cast<CustomBoard*>(usr_data);                                     // 以下程序实现供用户参考 ，实现单击pwr按键调整亮度               
+            // if(self->GetBacklight()->brightness() > 1)                                           // 如果亮度不为0
+            //     self->GetBacklight()->SetBrightness(1);                                          // 设置亮度为1         
+            // else
+            //     self->GetBacklight()->RestoreBrightness();                                       // 恢复原本亮度
+            // 短按无处理
+        }, this);
+        iot_button_register_cb(pwr_btn, BUTTON_LONG_PRESS_START, [](void* button_handle, void* usr_data) {
+            auto self = static_cast<CustomBoard*>(usr_data);
+            if(self->GetBacklight()->brightness() > 0) {
+                self->GetBacklight()->SetBrightness(0);
+                gpio_set_level(PWR_Control_PIN, false);
+            }
+            else {
+                self->GetBacklight()->RestoreBrightness();
+                gpio_set_level(PWR_Control_PIN, true);
+            }
+        }, this);
     }
-    
+
+
+    // 物联网初始化，添加对 AI 可见设备
+    void InitializeIot() {
+        auto& thing_manager = iot::ThingManager::GetInstance();
+        thing_manager.AddThing(iot::CreateThing("Speaker"));
+        thing_manager.AddThing(iot::CreateThing("Backlight"));
+    }
+
 public:
-    CustomBoard() :
-        boot_button_(BOOT_BUTTON_GPIO) {
+    CustomBoard() {   
         InitializeI2c();
         InitializeTca9554();
         InitializeSpi();
         Initializest77916Display();
-        InitializeButtons(); 
+        InitializeButtons();
+        InitializeIot();
         GetBacklight()->RestoreBrightness();
     }
 
     virtual AudioCodec* GetAudioCodec() override {
         static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, I2S_STD_SLOT_LEFT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN, I2S_STD_SLOT_RIGHT); // I2S_STD_SLOT_LEFT / I2S_STD_SLOT_RIGHT / I2S_STD_SLOT_BOTH
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, I2S_STD_SLOT_BOTH, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN, I2S_STD_SLOT_RIGHT); // I2S_STD_SLOT_LEFT / I2S_STD_SLOT_RIGHT / I2S_STD_SLOT_BOTH
 
         return &audio_codec;
     }
